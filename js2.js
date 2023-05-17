d9dbb0d9dae9c750dd327bfbb997382a8e920085
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-05-17 19:18:57 -0300

    Add files via upload
    A simple JS functions

---
+++ js2.js
@@ -0,0 +1,17 @@
+function soma(A,B) {
+    var A;
+    var B;
+    var soma_numero = A + B;
+    document.write(soma_numero);
+    return 0;
+}
+document.write("Exibindo a soma");
+soma(3,4);
+
+function soma() {
+    var soma_numero = "Essa função pode ser reutilizada";
+    document.write(soma_numero);
+}
+
+soma();
+
